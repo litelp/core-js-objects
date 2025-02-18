2bd785a05a78facacf3a010fdbb25db07d87cab6
litelp <lponomareva97@yandex.ru>
2025-02-18 19:56:28 +0400

    feat: add solution of task getJSON and  fromJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -205,8 +205,8 @@ function Rectangle(width, height) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { height: 10, width: 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
@@ -220,8 +220,8 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  return Object.setPrototypeOf(JSON.parse(json), proto);
 }
 
 /**
